b7fe55394ece6be78931c480f695a88c4c349e94
Jimena <jimeblue69@gmail.com>
2022-04-25 19:40:57 +0200

    Create custom hook to listen to data
    Co-authored-by: Reda Baha baha.redaaaa@gmail.com

---
+++ src/hooks/useListenItems.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+//firebase
+import { db } from '../lib/firebase';
+import { onSnapshot, collection, query, where } from 'firebase/firestore';
+import useToken from './useToken';
+
+const useListenItems = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [listeningError, setListeningError] = useState(null);
+  const { token } = useToken();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const ListRef = collection(db, 'List1');
+    const queryParam = query(ListRef, where('token', '=', token));
+    const unsb = onSnapshot(
+      queryParam,
+      (snapshot) => {
+        const result = [];
+        snapshot.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        });
+
+        setIsLoading(false);
+        setData(result);
+        setListeningError(null);
+      },
+      (error) => {
+        setIsLoading(false);
+        setListeningError('could not fetch data');
+      },
+    );
+
+    return () => unsb();
+  }, [token]);
+
+  return { data, isLoading, listeningError };
+};
+
+export default useListenItems;
