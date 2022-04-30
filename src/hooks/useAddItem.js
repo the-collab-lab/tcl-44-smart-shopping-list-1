d1b6370d56cd53ff7e7ab6a4a43bea609814de62
redapy <baha.redaaaa@gmail.com>
2022-04-30 15:01:58 +0000

    small fixes * move helper fucntions to the helper.js file
    Co-authored-by: Jimena Miguez <jimeblau@gmail.com>

--- src/hooks/useAddItem.js
+++ src/hooks/useAddItem.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import { db } from '../lib/firebase';
 import { addDoc, collection } from 'firebase/firestore';
 import useFetchItems from './useFetchItems';
+import {
+  checkDuplication,
+  showErrorMessage,
+  showSuccessMessage,
+} from '../utils/helpers';
 
 const useAddItem = (reference) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -14,73 +19,26 @@ const useAddItem = (reference) => {
     setIsLoading(true);
     try {
       const ListRef = collection(db, 'Lists');
-      let addedDocument;
-      checkDuplication(newItem)
-        ? showErrorMessage()
-        : (addedDocument = await addDoc(ListRef, {
-            itemName: newItem,
-            timeframe: parseInt(timeframe),
-            lastPurchased,
-            token,
-          }));
-      if (addedDocument) {
-        setIsLoading(false);
-        showSuccesMessage();
-        setError(false);
+
+      if (checkDuplication(items, newItem)) {
+        showErrorMessage(reference, setIsLoading, setDuplicateItemMessage);
+      } else {
+        const addedDocument = await addDoc(ListRef, {
+          itemName: newItem,
+          timeframe: parseInt(timeframe),
+          lastPurchased,
+          token,
+        });
+        if (addedDocument) {
+          setIsLoading(false);
+          showSuccessMessage(reference, setSuccessMessage);
+          setError(false);
+        }
       }
     } catch (e) {
       setError(true);
       setSuccessMessage(null);
     }
-  };
-
-  //-------------------------------------------------------------------------------------
-  // HELPER FUNCTIONS
-
-  //Check for duplication:
-  //get the existing items list from firebase
-  //loop through the existing items list to check if there is a match with current item
-  //set itemExists to true if duplication and return it
-
-  const checkDuplication = (newItem) => {
-    let itemExists = false;
-
-    items.forEach((itemObject) => {
-      //Remove punctuation of existing item with regex
-      let existingItem = itemObject.itemName;
-      let cleanExistingItem = existingItem.replace(/[\W|_]/g, '');
-
-      //Remove punctuation of current item with regex
-      let currentItem = newItem;
-      let cleanCurrentItem = currentItem.replace(/[\W|_]/g, '');
-
-      //Check for duplication while normalizin capitalization
-      if (cleanCurrentItem.toLowerCase() === cleanExistingItem.toLowerCase()) {
-        itemExists = true;
-      }
-    });
-    return itemExists;
-  };
-
-  //Set error message and erase it after 3 sec and focus text input
-  const showErrorMessage = () => {
-    setIsLoading(false);
-    setDuplicateItemMessage('Item already added. Try another one.');
-
-    setTimeout(() => {
-      reference.current.focus();
-      setDuplicateItemMessage('');
-    }, 3000);
-  };
-
-  // set the success message to infor the user that the item is added, and erase it after 3 sec
-  const showSuccesMessage = () => {
-    setSuccessMessage('Item added');
-
-    setTimeout(() => {
-      reference.current.focus();
-      setSuccessMessage('');
-    }, 3000);
   };
 
   return { addItem, isLoading, successMessage, error, duplicateItemMessage };
---
+++ src/utils/helpers.js
@@ -0,0 +1,48 @@
+//Check for duplication:
+//get the existing items list from firebase
+//loop through the existing items list to check if there is a match with current item
+//set itemExists to true if duplication and return it
+
+export const checkDuplication = (items, newItem) => {
+  let itemExists = false;
+  const regex = /[\W|_]/g;
+  items.forEach((itemObject) => {
+    //Remove punctuation of existing item with regex
+    const existingItem = itemObject.itemName;
+    const cleanExistingItem = existingItem.replace(regex, '');
+
+    //Remove punctuation of current item with regex
+    const currentItem = newItem;
+    const cleanCurrentItem = currentItem.replace(regex, '');
+
+    //Check for duplication while normalizin capitalization
+    if (cleanCurrentItem.toLowerCase() === cleanExistingItem.toLowerCase()) {
+      itemExists = true;
+    }
+  });
+  return itemExists;
+};
+
+//Set error message and erase it after 3 sec and focus text input
+export const showErrorMessage = (
+  reference,
+  setIsLoading,
+  setDuplicateItemMessage,
+) => {
+  setIsLoading(false);
+  setDuplicateItemMessage('Item already added. Try another one.');
+
+  setTimeout(() => {
+    reference.current.focus();
+    setDuplicateItemMessage('');
+  }, 3000);
+};
+
+// set the success message to infor the user that the item is added, and erase it after 3 sec
+export const showSuccessMessage = (reference, setSuccessMessage) => {
+  setSuccessMessage('Item added');
+  reference.current.focus();
+  setTimeout(() => {
+    setSuccessMessage('');
+  }, 3000);
+};
