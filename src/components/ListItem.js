23a25b944021c27e8dd936984d3c75da8b4a00e5
redapy <baha.redaaaa@gmail.com>
2022-05-03 18:48:45 +0000

    use etimatedTime in ListItem to record the estimated next purchase and how many times the item has benn purchased

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,48 +1,64 @@
 import { doc, updateDoc } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 import { useEffect, useState } from 'react';
+import { etimatedTime } from '../utils/estimates';
 
 const oneDayInSeconds = 86400;
 
 const style = {
-       listStyleType: 'none',
-       textAlign: 'left'
-}
+  listStyleType: 'none',
+  textAlign: 'left',
+};
 const ListItem = ({ itemData }) => {
-    const [checked, setChecked] = useState(itemData.lastPurchased !== null)
-    const nowMinusLastPurchased = () => {
-        return Math.floor(Date.now() / 1000 ) - itemData.lastPurchased.seconds
+  const [checked, setChecked] = useState(itemData.lastPurchased !== null);
+  const nowMinusLastPurchased = () => {
+    return Math.floor(Date.now() / 1000) - itemData.lastPurchased.seconds;
+  };
+  const wasPurchasedWithin24Hours = () => {
+    if (itemData.lastPurchased === null) {
+      return false;
     }
-    const wasPurchasedWithin24Hours = () => {
-        if (itemData.lastPurchased === null) {
-            return false
-        }
-        return nowMinusLastPurchased() <= oneDayInSeconds
+    return nowMinusLastPurchased() <= oneDayInSeconds;
+  };
+
+  useEffect(() => {
+    if (itemData.lastPurchased === null) {
+      return false;
     }
+    if (nowMinusLastPurchased() >= oneDayInSeconds) {
+      setChecked(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [itemData]);
 
-    useEffect(() => {
-        if (itemData.lastPurchased === null) {
-            return false
-        }
-        if (nowMinusLastPurchased() >= oneDayInSeconds) {
-            setChecked(false)
-        } 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [itemData]);
+  const handleChange = () => {
+    const docRef = doc(db, 'Lists', itemData.id);
+    if (
+      itemData.lastPurchased === null ||
+      nowMinusLastPurchased() >= oneDayInSeconds
+    ) {
+      updateDoc(docRef, {
+        lastPurchased: new Date(),
+        timeframe: etimatedTime(itemData),
+        totalPurchases: itemData.totalPurchases + 1,
+      });
+      setChecked(true);
+    }
+    console.log(etimatedTime(itemData));
+  };
 
-    const handleChange = () => {
-        const docRef = doc(db, "Lists", itemData.id)
-        if (itemData.lastPurchased === null || nowMinusLastPurchased() >= oneDayInSeconds)  {
-            updateDoc(docRef, {lastPurchased: new Date()});
-            setChecked(true)
-        } 
-        console.log(itemData.id)    
-    };
+  return (
+    <li style={style}>
+      <input
+        type="checkbox"
+        id="data.id"
+        disabled={wasPurchasedWithin24Hours()}
+        checked={checked}
+        onChange={handleChange}
+      />
+      <span>{itemData.itemName}</span>{' '}
+    </li>
+  );
+};
 
-    return (      
-        <li style={style}><input type="checkbox" id="data.id" disabled={wasPurchasedWithin24Hours()} checked={checked} onChange={handleChange} /><span> {itemData.itemName}</span> </li>
-    );
-   
-}
- 
 export default ListItem;
