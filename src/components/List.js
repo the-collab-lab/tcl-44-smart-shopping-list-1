dffdef8229f2592f5c09ec54947c7e5b593114f3
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-05-05 09:04:34 +0100

    simplify search in List component with setSearchTerm state
    Co-authored-by: Jimena Miguez <jimeblau@gmail.com>

--- src/components/List.js
+++ src/components/List.js
@@ -7,42 +7,31 @@ import { useState } from 'react';
 
 const List = () => {
   const { listeningError, isLoading, data } = useFetchItems();
-  const [searchInput, setSearchInput] = useState('');
-  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   if (localStorage.getItem('token') === null) {
     return <Navigate to="/" />;
   }
-
-  //Use conditional rendering to render either the filered items or the items list from firebase
-  //If searchInput.length === 0 map data and send each item data to the ListItem component
-  //If searchInput.length > 1 map filteredResults and send each item data to the ListItem component
 
   return (
     <>
       {listeningError && <p>{listeningError}</p>}
       {isLoading && <p>loading...</p>}
       {data && data.length === 0 && <WelcomingPrompt />}
-      <Search
-        setFilteredResults={setFilteredResults}
-        setSearchInput={setSearchInput}
-        searchInput={searchInput}
-      />
-      {data &&
-        searchInput.length === 0 &&
-        data.map((item) => (
-          <ul key={item.id}>
-            <ListItem itemData={item} />
-          </ul>
-        ))}
+      <Search setSearchTerm={setSearchTerm} />
 
       {data &&
-        searchInput.length > 1 &&
-        filteredResults.map((item) => (
-          <ul key={item.id}>
-            <ListItem itemData={item} />
-          </ul>
-        ))}
+        data
+          .filter((item) => {
+            return item.itemName
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase());
+          })
+          .map((item) => (
+            <ul key={item.id}>
+              <ListItem itemData={item} />
+            </ul>
+          ))}
     </>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import useFetchItems from '../hooks/useFetchItems';
 
-const Search = ({ searchInput, setSearchInput, setFilteredResults }) => {
+const Search = ({ setSearchTerm }) => {
   const { data } = useFetchItems();
 
   //Get list of items from firebase
@@ -8,19 +8,6 @@ const Search = ({ searchInput, setSearchInput, setFilteredResults }) => {
   //Filter list of items from firebase to see if they include the value from search input
   //Set filtered item to filteredResults state if searchInput is not empty,
   //otherwise set items list from firebase to filteredResults
-
-  const searchItems = (searchValue) => {
-    setSearchInput(searchValue);
-    if (searchInput !== '') {
-      const filteredItem = data.filter((item) => {
-        return item.itemName.toLowerCase().includes(searchInput.toLowerCase());
-      });
-      console.log(filteredItem);
-      setFilteredResults(filteredItem);
-    } else {
-      setFilteredResults(data);
-    }
-  };
 
   //Conditionally render the form to prevent it from showing when the list is empty
   return (
@@ -34,7 +21,7 @@ const Search = ({ searchInput, setSearchInput, setFilteredResults }) => {
             placeholder="Start typing here..."
             name="search-input"
             id="search-input"
-            onChange={(e) => searchItems(e.target.value)}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
         </form>
       )}
