80ff44053a0da2da71756db55869d9d04921a617
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-05-18 22:38:51 +0100

    removed old css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,11 @@ const Navbar = () => {
   } else
     return (
       <nav className="bg-yellow-200 fixed w-full bottom-0">
-        <ul className="flex justify-center gap-32 py-1">
+        <ul className="flex justify-center gap-32 py-1 ">
           <li>
             <NavLink to="/list" className="flex flex-col items-center"><TiShoppingCart className="text-4xl" /><span>List</span></NavLink>
           </li>
-          <li >
+          <li>
             <NavLink to="/add-item" className="flex flex-col items-center"><TiPlusOutline className="text-4xl" /><span>Add Item</span></NavLink>
           </li>
         </ul>
