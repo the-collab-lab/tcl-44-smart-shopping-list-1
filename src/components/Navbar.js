e2d318468170bbfa4a0bea84e2dd1eab77cf1ed2
Jimena <jimeblue69@gmail.com>
2022-05-23 18:09:16 +0200

    Add z-index to navbar to fix bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import useToken from '../hooks/useToken';
-import {TiShoppingCart, TiPlusOutline} from "react-icons/ti"
+import { TiShoppingCart, TiPlusOutline } from 'react-icons/ti';
 
 const Navbar = () => {
   const { token } = useToken();
@@ -9,13 +9,19 @@ const Navbar = () => {
     return null;
   } else
     return (
-      <nav className="bg-yellow-200 fixed w-full bottom-0">
+      <nav className="bg-yellow-200 fixed w-full bottom-0 z-40">
         <ul className="flex justify-center gap-32 py-1 ">
           <li>
-            <NavLink to="/list" className="flex flex-col items-center"><TiShoppingCart className="text-4xl" /><span>List</span></NavLink>
+            <NavLink to="/list" className="flex flex-col items-center">
+              <TiShoppingCart className="text-4xl" />
+              <span>List</span>
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/add-item" className="flex flex-col items-center"><TiPlusOutline className="text-4xl" /><span>Add Item</span></NavLink>
+            <NavLink to="/add-item" className="flex flex-col items-center">
+              <TiPlusOutline className="text-4xl" />
+              <span>Add Item</span>
+            </NavLink>
           </li>
         </ul>
       </nav>
