07ea90272f4f59aa4aa6548a8ddbb1732e3f6e0b
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2022-05-24 17:16:22 +0100

    Fix spelling and style list page

--- src/components/WelcomingPrompt.js
+++ src/components/WelcomingPrompt.js
@@ -3,13 +3,13 @@ import Button from '../components/Button';
 
 const WelcomingPrompt = () => {
   return (
-    <>
-      <p>Welcome to your shoping list. Your list is currenctly empty!</p>
+    <section className="flex flex-col text-xl text-center gap-4 pt-8 sm: w-2/3">
+      <p>Welcome to your shopping list. Your list is currently empty!</p>
       <p>Please click the button below to add an item.</p>
       <Link to="/add-item">
         <Button text="add" width={'w-40'} />
       </Link>
-    </>
+    </section>
   );
 };
 
