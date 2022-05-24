2ad4a7d1ee686c17cfdfb8dcd3c55c099f97ff9d
Jimena <jimeblue69@gmail.com>
2022-05-24 18:33:16 +0200

    Add width styles to header´s title in welcome page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ const Header = ({ title, imageSrc, isHome }) => {
   let titleStyles;
   if (isHome) {
     headerStyles = 'bg-yellow-400 w-full text-center relative h-32 mb-32';
-    titleStyles = ' font-bold text-2xl w-1/4 mx-auto mt-4';
+    titleStyles =
+      ' font-bold text-2xl w-1/4 mx-auto mt-4 w-1/2 lg:w-1/4 2xl:w-full';
   } else {
     headerStyles = 'bg-yellow-400 w-full flex justify-center';
     titleStyles = 'font-bold text-2xl mt-4 mr-4';
