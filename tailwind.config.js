9467b08556fd4f9231cec16672ffdbf5d7509ff8
redapy <baha.redaaaa@gmail.com>
2022-05-25 23:44:27 +0100

    fix bug: install tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/custom-forms')],
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class', // only generate classes
+    }),
+  ],
 };
