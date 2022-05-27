a56928fb82e4617106c81fae0408215d7e88b058
redapy <baha.redaaaa@gmail.com>
2022-05-27 21:27:54 +0100

    add background for the app
    Co-authored-by: Jimena Miguez <jimeblau@gmail.com>
    Co-authored-by: Debbie Dann <d3bbie.dann@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,11 +6,59 @@ import List from './components/List';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import TokenProvider from './context/tokenContext';
+//images
+import lemon from '../src/assets/background-images/lemon.svg';
+import apple from '../src/assets/background-images/apple.svg';
+import pear from '../src/assets/background-images/pear.svg';
+import pineapple from '../src/assets/background-images/pineapple.svg';
 
 function App() {
   return (
     <TokenProvider>
-      <div className="flex flex-col items-center text-stone-700">
+      <div className="flex flex-col items-center text-stone-700 w-full lg:w-1/2 min-h-screen mx-auto shadow-xl bg-white">
+        <img
+          src={lemon}
+          width="250px"
+          height="250px"
+          alt="background apple"
+          className="fixed top-10  left-60 -z-10"
+        />
+        <img
+          src={apple}
+          width="250px"
+          height="250px"
+          alt="background apple"
+          className="fixed top-10  right-52 -z-10 origin-center rotate-45"
+        />
+        <img
+          src={apple}
+          width="250px"
+          height="250px"
+          alt="background apple"
+          className="fixed inset-y-1/3 -left-20  -z-10 origin-center rotate-45"
+        />
+        <img
+          src={lemon}
+          width="250px"
+          height="250px"
+          alt="background apple"
+          className="fixed inset-y-1/3 -right-32  -z-10 "
+        />
+
+        <img
+          src={pear}
+          width="250px"
+          height="250px"
+          alt="background apple"
+          className="fixed bottom-20  left-52 -z-10 origin-center -rotate-45"
+        />
+        <img
+          src={pineapple}
+          width="250px"
+          height="250px"
+          alt="background apple"
+          className="fixed bottom-16  right-56 -z-10 origin-center rotate-45"
+        />
         <BrowserRouter>
           <Navbar />
           <Routes>
